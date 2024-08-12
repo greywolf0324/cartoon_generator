841a813ded49fc39ffe6ca3667324f3a71d84be4
greywolf0324 <johngrey0324@gmail.com>
2024-08-12 12:21:58 +0200

    apikey update

--- apikey.py
+++ apikey.py
@@ -1,5 +1,5 @@
-OPENAI_API_KEY = 'tl.t1B2FRyVUMSJtL97VnKbU4CmclGKgNtpbpzRI2HKRxPT[LOq'
-
+# OPENAI_API_KEY = 'tl.t1B2FRyVUMSJtL97VnKbU4CmclGKgNtpbpzRI2HKRxPT[LOq'
+OPENAI_API_KEY = 'tl.hfzOu9{UK2oEvXl6j2JnU4CmclGKubtoG6ZPWwI8yeVCwfhp'
 def encrypt(api_key):
     return ''.join(chr(ord(char) + 1) for char in api_key)
 
