0a32270e6f9fa43d98124c26cdcfbfb378895ff8
greywolf0324 <johngrey0324@gmail.com>
2024-08-22 03:45:28 +0200

    openai apikey change

--- apikey.py
+++ apikey.py
@@ -1,5 +1,6 @@
 # OPENAI_API_KEY = 'tl.t1B2FRyVUMSJtL97VnKbU4CmclGKgNtpbpzRI2HKRxPT[LOq'
-OPENAI_API_KEY = 'tl.hfzOu9{UK2oEvXl6j2JnU4CmclGKubtoG6ZPWwI8yeVCwfhp'
+# OPENAI_API_KEY = 'tl.hfzOu9{UK2oEvXl6j2JnU4CmclGKubtoG6ZPWwI8yeVCwfhp'
+OPENAI_API_KEY = 'tl.qspk.ByFdkHX{z1sxnmYL.kisGsSrjP9km2F7:JzJv[lyBz.otp`hF6VFo:b48TU4CmclGKO7PTsUsMXX9JhHeiT:58X7yyn7Te:JyxK7hILHFO64CfsVnnHRPq3LQjlB'
 def encrypt(api_key):
     return ''.join(chr(ord(char) + 1) for char in api_key)
 
